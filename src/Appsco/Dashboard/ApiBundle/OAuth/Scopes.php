5a71b7cd70ee214ab88d5741216b8de78f3373c8
Marjan Hrzic <marjan.hrzic@boutsourcing.com>
2015-03-20 16:27:39 +0100

    Added scopes

--- src/Appsco/Dashboard/ApiBundle/OAuth/Scopes.php
+++ src/Appsco/Dashboard/ApiBundle/OAuth/Scopes.php
@@ -5,6 +5,9 @@ namespace Appsco\Dashboard\ApiBundle\OAuth;
 final class Scopes
 {
     const PROFILE_READ = 'profile_read';
+    const DASHBOARD_LIST = 'dashboard_list';
+    const DASHBOARD_ICON_LIST = 'dashboard_icon_list';
+    const DASHBOARD_APPLICATION_ADD = 'dashboard_application_add';
 
     private function __construct() { }
 }
